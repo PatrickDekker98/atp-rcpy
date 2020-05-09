7cd35077c6b0432127f8a338e85bb818f565e457
Patrick Dekker <patrick_dekker@hotmail.com>
2020-05-09 22:34:15 +0200

    added propper README

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
-# rcpy interpreter 
+# RCPY
+RCPY is an esotheric programming language that is supposed to read as a recipe. It turned out te be something magical, where you can pass your 3 onions to cook potatos and end up with 100 potatos. This is really your party, but you have to program you own cake baking function... Don't tell your guests you made it with yogurt instead of whipped cream, they won't notice but still. 
+How does it do this? 
+By butchering the english language ofcourse! 
+```
+while: potatos < 100; (
+    potatos + 1 potatos
+)
+```
+^ this is verry correct syntax
+## Current features:
+Currently rcpy has the following features.
+### literal arithmetic expressions
+|Example|Explanation
+ | :- | :- 
+ | 12| a literal integer
+ | onion| a variable
+### Binary arithmetic expressions
+
+| Operator        | Example           | Explanation  |
+| :------------ |:-------------| :-----|
+| +      | ``` onios + 2 ```|adds an arithmetic to the preceding one|
+| - | ``` onions - 3```|   subtracts an arithmetic from the preceding one|
+| * | ```onions * onions```| multiplies an arithmetic with the preceding one |
+|/  | ``` 100 / onions```|divides an arithmetic with the preceding one |
+### Relational expressions
+|Operator|Example|Explanation|
+|:-|:-|:-|
+|==|```onions == scallions```|returns true if onions is equal to scallions false otherways
+|<|```onions == scallions```|returns true if onions is smaller than scallions false otherways
+|>|```onions == scallions```|returns true if onions is greater than scallions false otherways
+|<=|```onions == scallions```|returns true if onions is smaller or equal to scallions false otherways
+|>=|```onions == scallions```|returns true if onions is greater of equal to scallions false otherways
+### Assign statements
+Artimethic values can be assigned to variables.
+```
+2 onions 
+3 scallions
+```
+Above here the value of '2' is assigned to onions and '3' is assigned to scallions. As you can see the assingment operator is not explicitly visible, that is because it is implicid. Also, as you can see assignments are also processed from right to left as opposed from normal languages. This allowes one to do... unconvential things.
+```
+scallions are better than onions and you cannot change my mind
+```
+^ This assigns all words in 'are better than onions and you cannot change my mind' to the same value as 'scallions', witch ironicaly disproves the statement
+### If statements
+As noted previously onions now equal scallions.
+```
+if: onions equal scallions; (
+	onions + scallions onions
+)
+```
+^ Following this onions now equals 6.
+> sadly else () is not yet implemented
+### While loops
+While while loops work. 
+```
+while: potatos < 100; (
+    potatos + 1 potatos
+)
+```
+### Return 
+Arthimetics can be returned with two keywords; ```return``` and ```serve```.
+### Functions
+This language has functions! Functions are declared by telling how to do something, with the keyword 'howto'.
+```
+howto cook: bread, butter ; (
+    butter + bread soup then
+    serve soup
+)
+```
+Functions are then called like this:
+```
+:potatos, onions; cook potatos
+```
+The parameters are specified left of the function call.
+## Limitations
+### Negativity
+Negative ingedients don't exist, that would be stupid...
+But if you do need them, don't thell anyone I told you this:
+```
+0 - 5 broccoli
+``` 
+it will equal -5 broccoli.
+### Errors
+If shit hits the fan, the interpreter won't help you.
+
+## Comming features
+**floats**, don't exist yet
+**errors**, will probaply be in the form of a certain scottish chef telling you you are an idiot sandwitch, but wil at least tell where you failed.
+**else**, and maybe even else if.
+ ## Credits
+ Idea  is credited to my brother, Colin.
--- run.py
+++ run.py
@@ -1,5 +1,8 @@
+from typing import Iterable, List, Tuple, Callable, TypeVar
 import nodes
 import operator_funcs
+
+T = TypeVar('T')
 
 """
 @brief program state
@@ -25,7 +28,7 @@ class error():
     def __repr__(self):
         return self.__str__()
 
-def smart_divide(f):
+def smart_divide(f : Callable[[T, T], float]) -> float:
     def inner(a, b):
         if b == 0:
             return error("you fucking donkey!")
@@ -49,50 +52,51 @@ bin_operator_dict = {
         '/': smart_divide(operator_funcs.divide)
         }
 
-def eval_int_expr(int_exp : nodes.int_a_expression, ps : program_state):
+def eval_int_expr(int_exp : nodes.int_a_expression, ps : program_state) -> int:
     return int_exp.i
 
-def eval_float_expr(float_expr : nodes.float_a_expression, ps : program_state):
+def eval_float_expr(float_expr : nodes.float_a_expression, ps : program_state) -> float:
     return float_expr.f
 
-def eval_name_expr(name_expr : nodes.name_a_expression, ps : program_state):
+def eval_name_expr(name_expr : nodes.name_a_expression, ps : program_state) -> T:
     return ps.vals[name_expr.name]
 
-def eval_bin_op_expr(bin_op : nodes.bin_op_a_expression, ps : program_state):
+def eval_bin_op_expr(bin_op : nodes.bin_op_a_expression, ps : program_state) -> T:
     return bin_operator_dict[bin_op.op](eval_node(bin_op.lhs, ps), eval_node(bin_op.rhs, ps))
 
-def eval_assign_statement(ass_op : nodes.assign_statement, ps : program_state):
+def eval_assign_statement(ass_op : nodes.assign_statement, ps : program_state) -> T:
     ps.vals[ass_op.name.name] = eval_node(ass_op.a_exp, ps)
     return ps
 
-def eval_rel_expr(rel_op: nodes.bin_op_a_expression, ps : program_state):
+def eval_rel_expr(rel_op: nodes.bin_op_a_expression, ps : program_state) -> bool:
     return rel_operator_dict[rel_op.op](eval_node(rel_op.lhs, ps), eval_node(rel_op.rhs, ps))
 
-def eval_if_stat(if_stat: nodes.if_statement, ps : program_state):
+def eval_if_stat(if_stat: nodes.if_statement, ps : program_state) -> program_state:
     if eval_node(if_stat.cond, ps):
         return run(if_stat.if_true, ps)
     return ps
 
-def eval_while_stat(while_stat: nodes.while_statement, ps : program_state):
+def eval_while_stat(while_stat: nodes.while_statement, ps : program_state) -> program_state:
     if eval_node(while_stat.cond, ps):
         return eval_while_stat(while_stat, run(while_stat.loop))
     return ps
 
-def eval_return_stat(return_stat: nodes.return_statement, ps : program_state):
+def eval_return_stat(return_stat: nodes.return_statement, ps : program_state) -> program_state:
     return eval_node(return_stat.exp, ps)
 
-def eval_function_dec(func_dec: nodes.function_dec_statement, ps : program_state):
+def eval_function_dec(func_dec: nodes.function_dec_statement, ps : program_state) -> program_state:
     ps.funcs[func_dec.name.name] = func_dec.a_exp
     return ps
 
-def zip_with(list_one : list, list_two : list, f : callable):
+
+def zip_with(list_one : List[T], list_two : List[T], f : Callable[[T], T]) -> T:
     if len(list_one) == 0 or len(list_two) == 0:
         return []
     one = list_one.pop(0)
     two = list_two.pop(0)
     return [f(one, two)] + zip_with(list_one, list_two, f)
 
-def eval_function_call(func_call: nodes.function_a_call, ps : program_state):
+def eval_function_call(func_call: nodes.function_a_call, ps : program_state) -> T:
     func = ps.funcs[func_call.name]
     params = zip(func.params)
     params = zip_with(func.params, func_call.params, (lambda one, two : nodes.assign_statement(one, two)))
@@ -113,10 +117,10 @@ evaluate_dict = {
         nodes.function_a_call: eval_function_call
         }
 
-def eval_node(nod : nodes.node, ps : program_state):
+def eval_node(nod : nodes.node, ps : program_state) -> T:
     return evaluate_dict[type(nod)](nod, ps)
 
-def run(ast, ps = program_state()):
+def run(ast : List[Tuple[str, str]], ps : program_state = program_state()  ) -> T:
     if len(ast) == 0 or type(ps) != program_state:
         return ps 
     ps = eval_node(ast[0], ps) 
